a974dac32c257eafb8a48a60c08fa90b30f38c16
FinOCE <me@itsf.in>
2024-09-02 11:01:51 +1000

    Create remaining command option types and template tests

--- src/Discordfs.Tests.Unit/Commands/OptionsTests.fs
+++ src/Discordfs.Tests.Unit/Commands/OptionsTests.fs
@@ -6,7 +6,98 @@ open Modkit.Discordfs.Types
 [<TestClass>]
 type OptionsTests () =
     [<TestMethod>]
+    [<Ignore>]
+    member _.SubCommand_FindsCorrectSubCommand () =
+        Assert.Fail() // TODO
+
+    [<TestMethod>]
+    [<Ignore>]
+    member _.SubCommand_ReturnsNoneIfOptionIncorrectType () =
+        Assert.Fail() // TODO
+
+    [<TestMethod>]
+    [<Ignore>]
+    member _.SubCommand_ReturnsNoneIfOptionNonExistent () =
+        Assert.Fail() // TODO
+
+    [<TestMethod>]
+    [<Ignore>]
+    member _.SubCommandGroup_FindsCorrectSubCommandGroup () =
+        Assert.Fail() // TODO
+
+    [<TestMethod>]
+    [<Ignore>]
+    member _.SubCommandGroup_ReturnsNoneIfOptionIncorrectType () =
+        Assert.Fail() // TODO
+
+    [<TestMethod>]
+    [<Ignore>]
+    member _.SubCommandGroup_ReturnsNoneIfOptionNonExistent () =
+        Assert.Fail() // TODO
+
+    [<TestMethod>]
+    [<Ignore>]
+    member _.String_FindsCorrectString () =
+        Assert.Fail() // TODO
+
+    [<TestMethod>]
+    [<Ignore>]
+    member _.String_ReturnsNoneIfOptionIncorrectType () =
+        Assert.Fail() // TODO
+
+    [<TestMethod>]
+    [<Ignore>]
+    member _.String_ReturnsNoneIfOptionNonExistent () =
+        Assert.Fail() // TODO
+
+    [<TestMethod>]
+    [<Ignore>]
+    member _.Integer_FindsCorrectInteger () =
+        Assert.Fail() // TODO
+
+    [<TestMethod>]
+    [<Ignore>]
+    member _.Integer_ReturnsNoneIfOptionIncorrectType () =
+        Assert.Fail() // TODO
+
+    [<TestMethod>]
+    [<Ignore>]
+    member _.Integer_ReturnsNoneIfOptionNonExistent () =
+        Assert.Fail() // TODO
+
+    [<TestMethod>]
+    [<Ignore>]
+    member _.Boolean_FindsCorrectBoolean () =
+        Assert.Fail() // TODO
+
+    [<TestMethod>]
+    [<Ignore>]
+    member _.Boolean_ReturnsNoneIfOptionIncorrectType () =
+        Assert.Fail() // TODO
+
+    [<TestMethod>]
+    [<Ignore>]
+    member _.Boolean_ReturnsNoneIfOptionNonExistent () =
+        Assert.Fail() // TODO
+
+    [<TestMethod>]
+    [<Ignore>]
+    member _.User_FindsCorrectUser () =
+        Assert.Fail() // TODO
+
+    [<TestMethod>]
+    [<Ignore>]
+    member _.User_ReturnsNoneIfOptionIncorrectType () =
+        Assert.Fail() // TODO
+
+    [<TestMethod>]
+    [<Ignore>]
+    member _.User_ReturnsNoneIfOptionNonExistent () =
+        Assert.Fail() // TODO
+
+    [<TestMethod>]
     member _.Channel_FindsCorrectChannel () =
+        // Arrange
         let name = "channel"
         let value = "channelId"
 
@@ -18,12 +109,18 @@ type OptionsTests () =
             )
         ]
 
-        match options with
-        | Options.Channel name channelId -> Assert.AreEqual(value, channelId)
-        | _ -> failwith "Could not find channel"
+        // Act
+        let channelId =
+            match options with
+            | Options.Channel name channelId -> channelId
+            | _ -> failwith "Could not find channel"
+
+        // Assert
+        Assert.AreEqual(value, channelId)
 
     [<TestMethod>]
     member _.Channel_ReturnsNoneIfOptionIncorrectType () =
+        // Arrange
         let name = "channel"
 
         let options = [
@@ -34,14 +131,85 @@ type OptionsTests () =
             )
         ]
 
-        match options with
-        | Options.Channel "channel" _ -> failwith "Unexpectedly found non-existent channel"
-        | _ -> ()
+        // Act
+        let res =
+            match options with
+            | Options.Channel "channel" _ -> failwith "Unexpectedly found non-existent channel"
+            | _ -> ()
+
+        // Assert
+        Assert.IsNull(res)
 
     [<TestMethod>]
     member _.Channel_ReturnsNoneIfOptionNonExistent () =
+        // Arrange
         let options: CommandInteractionDataOption list = []
         
-        match options with
-        | Options.Channel "channel" _ -> failwith "Unexpectedly found non-existent channel"
-        | _ -> ()
+        // Act
+        let res =
+            match options with
+            | Options.Channel "channel" _ -> failwith "Unexpectedly found non-existent channel"
+            | _ -> ()
+
+        // Assert
+        Assert.IsNull(res)
+
+    [<TestMethod>]
+    [<Ignore>]
+    member _.Role_FindsCorrectRole () =
+        Assert.Fail() // TODO
+
+    [<TestMethod>]
+    [<Ignore>]
+    member _.Role_ReturnsNoneIfOptionIncorrectType () =
+        Assert.Fail() // TODO
+
+    [<TestMethod>]
+    [<Ignore>]
+    member _.Role_ReturnsNoneIfOptionNonExistent () =
+        Assert.Fail() // TODO
+
+    [<TestMethod>]
+    [<Ignore>]
+    member _.Mentionable_FindsCorrectMentionable () =
+        Assert.Fail() // TODO
+
+    [<TestMethod>]
+    [<Ignore>]
+    member _.Mentionable_ReturnsNoneIfOptionIncorrectType () =
+        Assert.Fail() // TODO
+
+    [<TestMethod>]
+    [<Ignore>]
+    member _.Mentionable_ReturnsNoneIfOptionNonExistent () =
+        Assert.Fail() // TODO
+
+    [<TestMethod>]
+    [<Ignore>]
+    member _.Number_FindsCorrectNumber () =
+        Assert.Fail() // TODO
+
+    [<TestMethod>]
+    [<Ignore>]
+    member _.Number_ReturnsNoneIfOptionIncorrectType () =
+        Assert.Fail() // TODO
+
+    [<TestMethod>]
+    [<Ignore>]
+    member _.Number_ReturnsNoneIfOptionNonExistent () =
+        Assert.Fail() // TODO
+
+    [<TestMethod>]
+    [<Ignore>]
+    member _.Attachment_FindsCorrectAttachment () =
+        Assert.Fail() // TODO
+
+    [<TestMethod>]
+    [<Ignore>]
+    member _.Attachment_ReturnsNoneIfOptionIncorrectType () =
+        Assert.Fail() // TODO
+
+    [<TestMethod>]
+    [<Ignore>]
+    member _.Attachment_ReturnsNoneIfOptionNonExistent () =
+        Assert.Fail() // TODO
--- src/Discordfs/Commands/Options.fs
+++ src/Discordfs/Commands/Options.fs
@@ -3,6 +3,66 @@
 open Modkit.Discordfs.Types
 
 module Options =
+    let (|SubCommand|_|) (name: string) (options: CommandInteractionDataOption list) =
+        options |> List.tryPick (fun option ->
+            match option with
+            | ({
+                Options = Some options;
+                Type = ApplicationCommandOptionType.SUB_COMMAND;
+            }) when option.Name = name -> Some options
+            | _ -> None
+        )
+
+    let (|SubCommandGroup|_|) (name: string) (options: CommandInteractionDataOption list) =
+        options |> List.tryPick (fun option ->
+            match option with
+            | ({
+                Options = Some options;
+                Type = ApplicationCommandOptionType.SUB_COMMAND_GROUP;
+            }) when option.Name = name -> Some options
+            | _ -> None
+        )
+
+    let (|String|_|) (name: string) (options: CommandInteractionDataOption list) =
+        options |> List.tryPick (fun option ->
+            match option with
+            | ({
+                Value = Some (CommandInteractionDataOptionValue.String value);
+                Type = ApplicationCommandOptionType.STRING;
+            }) when option.Name = name -> Some value
+            | _ -> None
+        )
+
+    let (|Integer|_|) (name: string) (options: CommandInteractionDataOption list) =
+        options |> List.tryPick (fun option ->
+            match option with
+            | ({
+                Value = Some (CommandInteractionDataOptionValue.Int value);
+                Type = ApplicationCommandOptionType.INTEGER;
+            }) when option.Name = name -> Some value
+            | _ -> None
+        )
+
+    let (|Boolean|_|) (name: string) (options: CommandInteractionDataOption list) =
+        options |> List.tryPick (fun option ->
+            match option with
+            | ({
+                Value = Some (CommandInteractionDataOptionValue.Bool value);
+                Type = ApplicationCommandOptionType.BOOLEAN;
+            }) when option.Name = name -> Some value
+            | _ -> None
+        )
+
+    let (|User|_|) (name: string) (options: CommandInteractionDataOption list) =
+        options |> List.tryPick (fun option ->
+            match option with
+            | ({
+                Value = Some (CommandInteractionDataOptionValue.String userId);
+                Type = ApplicationCommandOptionType.USER;
+            }) when option.Name = name -> Some userId
+            | _ -> None
+        )
+
     let (|Channel|_|) (name: string) (options: CommandInteractionDataOption list) =
         options |> List.tryPick (fun option ->
             match option with
@@ -12,5 +72,43 @@ module Options =
             }) when option.Name = name -> Some channelId
             | _ -> None
         )
-        
-        // TODO: Create versions of above for all option types
+
+    let (|Role|_|) (name: string) (options: CommandInteractionDataOption list) =
+        options |> List.tryPick (fun option ->
+            match option with
+            | ({
+                Value = Some (CommandInteractionDataOptionValue.String roleId);
+                Type = ApplicationCommandOptionType.ROLE;
+            }) when option.Name = name -> Some roleId
+            | _ -> None
+        )
+
+    let (|Mentionable|_|) (name: string) (options: CommandInteractionDataOption list) =
+        options |> List.tryPick (fun option ->
+            match option with
+            | ({
+                Value = Some (CommandInteractionDataOptionValue.String mentionableId);
+                Type = ApplicationCommandOptionType.MENTIONABLE;
+            }) when option.Name = name -> Some mentionableId
+            | _ -> None
+        )
+
+    let (|Number|_|) (name: string) (options: CommandInteractionDataOption list) =
+        options |> List.tryPick (fun option ->
+            match option with
+            | ({
+                Value = Some (CommandInteractionDataOptionValue.Double value);
+                Type = ApplicationCommandOptionType.NUMBER;
+            }) when option.Name = name -> Some value
+            | _ -> None
+        )
+
+    let (|Attachment|_|) (name: string) (options: CommandInteractionDataOption list) =
+        options |> List.tryPick (fun option ->
+            match option with
+            | ({
+                Value = Some (CommandInteractionDataOptionValue.String attachmentId);
+                Type = ApplicationCommandOptionType.ATTACHMENT;
+            }) when option.Name = name -> Some attachmentId
+            | _ -> None
+        )
