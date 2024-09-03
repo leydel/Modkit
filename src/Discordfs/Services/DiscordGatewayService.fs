8c37f8aed1b72b0e9a12ef25b1de048f63515567
FinOCE <me@itsf.in>
2024-09-03 23:16:02 +1000

    Add internal gateway send method and receive loop

--- src/Discordfs/Services/DiscordGatewayService.fs
+++ src/Discordfs/Services/DiscordGatewayService.fs
@@ -3,6 +3,7 @@
 open Modkit.Discordfs.Types
 open System
 open System.Net.WebSockets
+open System.Text
 open System.Threading
 open System.Threading.Tasks
 
@@ -11,6 +12,8 @@ type IDiscordGatewayService =
         unit ->
         Task<unit>
 
+    // TODO: Define possible events here, like how done in DiscordHttpService
+
 type DiscordGatewayService (discordHttpService: IDiscordHttpService) =
     static member GatewayVersion: string = "10"
 
@@ -18,13 +21,8 @@ type DiscordGatewayService (discordHttpService: IDiscordHttpService) =
 
     static member GatewayCompression: GatewayCompression option = None
 
-    member val private _cts: CancellationTokenSource =
-        let cts = new CancellationTokenSource ()
-        cts.CancelAfter(TimeSpan.FromSeconds 5)
-        cts
-
     member val private _ws: ClientWebSocket option = None with get, set
-
+    
     member _.GetGatewayUrl () = task {
         let! gateway = (discordHttpService.GetGateway
             DiscordGatewayService.GatewayVersion
@@ -34,23 +32,53 @@ type DiscordGatewayService (discordHttpService: IDiscordHttpService) =
         return gateway.Url
     }
 
+    member this.Send (message: string) = task {
+        // TODO: Correctly handle sending events (https://discord.com/developers/docs/topics/gateway#sending-events)
+
+        let cts = new CancellationTokenSource ()
+        cts.CancelAfter(TimeSpan.FromSeconds 5)
+
+        match this._ws with
+        | None -> ()
+        | Some ws ->
+            let buffer = Encoding.UTF8.GetBytes message
+
+            let rec loop buffer offset = task {
+                let count = 1024
+                let segment = ArraySegment(buffer, offset, count)
+                let isEndOfMessage = offset + count >= buffer.Length
+
+                do! ws.SendAsync(segment, WebSocketMessageType.Text, isEndOfMessage, cts.Token)
+            }
+
+            do! loop buffer 0
+    }
+
     interface IDiscordGatewayService with 
         member this.Connect () = task {
             let! url = this.GetGatewayUrl()
 
-            let ws = new ClientWebSocket()
-            do! ws.ConnectAsync(Uri url, this._cts.Token)
+            let cts = new CancellationTokenSource ()
+            cts.CancelAfter(TimeSpan.FromSeconds 5)
 
+            let ws = new ClientWebSocket()
+            do! ws.ConnectAsync(Uri url, cts.Token)
             this._ws <- Some ws
+
+            let rec loop buffer = task {
+                let! res = ws.ReceiveAsync(ArraySegment buffer, CancellationToken.None)
+
+                match res.MessageType with
+                | WebSocketMessageType.Close -> ()
+                | _ ->
+                    let message = Encoding.UTF8.GetString(buffer, 0, res.Count)
+
+                    // TODO: Handle received message here
+
+                    return! loop buffer
+            }
+
+            return! loop (Array.zeroCreate<byte> 4096)
         }
 
-    interface IDisposable with
-        member this.Dispose () =
-            this._cts.Dispose()
-
-            match this._ws with
-            | None -> ()
-            | Some ws -> ws.Dispose()
-
-    // TODO: Handle sending/receiving events and the connection lifecycle: https://discord.com/developers/docs/topics/gateway#connection-lifecycle
-    
+        // TODO: Handle lifecycle (https://discord.com/developers/docs/topics/gateway#connection-lifecycle)
