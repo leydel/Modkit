fc7eea56529898f9c006fb8cd2b4539a05798782
FinOCE <me@itsf.in>
2024-09-08 17:46:57 +1000

    Create api function app and unit test projects

---
+++ src/Api/Program.fs
@@ -0,0 +1,23 @@
+﻿open Microsoft.Extensions.Configuration
+open Microsoft.Extensions.DependencyInjection
+open Microsoft.Extensions.Hosting
+open System.IO
+
+HostBuilder()
+    .ConfigureFunctionsWorkerDefaults()
+    .ConfigureAppConfiguration(fun builder ->
+        builder
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("local.settings.json", true)
+            .AddEnvironmentVariables()
+        |> ignore
+    )
+    .ConfigureServices(fun services -> 
+        services
+            .AddApplicationInsightsTelemetryWorkerService()
+        |> ignore
+    )
+    .Build()
+    .RunAsync()
+|> Async.AwaitTask
+|> Async.RunSynchronously
