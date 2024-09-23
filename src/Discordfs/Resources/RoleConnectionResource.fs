707b524f6c9c764d242d97b48053d1164c1b643d
FinOCE <me@itsf.in>
2024-09-23 10:07:36 +1000

    Refactor role connections resource using req ce

--- src/Discordfs/Resources/RoleConnectionResource.fs
+++ src/Discordfs/Resources/RoleConnectionResource.fs
@@ -2,112 +2,55 @@
 
 open Modkit.Discordfs.Common
 open Modkit.Discordfs.Types
+open Modkit.Discordfs.Utils
 open System.Collections.Generic
-open System.Net.Http
 open System.Threading.Tasks
+
+type UpdateApplicationRoleConnectionMetadataRecords (
+    ``type``: ApplicationRoleConnectionMetadataType,
+    key: string,
+    name: string,
+    description: string,
+    ?name_localizations: IDictionary<string, string>,
+    ?description_localizations: IDictionary<string, string>
+) =
+    inherit Payload(Json) with
+        override _.Serialize () = json {
+            required "type" ``type``
+            required "key" key
+            required "name" name
+            optional "name_localizations" name_localizations
+            required "description" description
+            optional "description_localizations" description_localizations
+        }
+
+    // TODO: Double check this is valid (specific payload not documented)
 
 type IRoleConnectionResource =
     abstract member GetApplicationRoleConnectionMetadataRecords:
         applicationId: string ->
         Task<ApplicationRoleConnectionMetadata list>
         
-    // TODO: Double check the payload type for this
     abstract member UpdateApplicationRoleConnectionMetadataRecords:
         applicationId: string ->
-        ``type``: ApplicationRoleConnectionMetadataType ->
-        key: string ->
-        name: string ->
-        nameLocalizations: IDictionary<string, string> option ->
-        description: string ->
-        descriptionLocalizations: IDictionary<string, string> option ->
+        content: UpdateApplicationRoleConnectionMetadataRecords ->
         Task<ApplicationRoleConnectionMetadata list>
-        
-    abstract member GetCurrentUserApplicationRoleConnection:
-        applicationId: string ->
-        oauth2AccessToken: string ->
-        Task<ApplicationRoleConnection>
 
-    // TODO: Double check the payload type for this
-    abstract member UpdateCurrentUserApplicationRoleConnection:
-        applicationId: string ->
-        oauth2AccessToken: string ->
-        platformName: string option ->
-        platformUsername: string option ->
-        ``type``: ApplicationRoleConnectionMetadataType ->
-        key: string ->
-        name: string ->
-        nameLocalizations: IDictionary<string, string> option ->
-        description: string ->
-        descriptionLocalizations: IDictionary<string, string> option ->
-        Task<ApplicationRoleConnection>
-
-    // TODO: CurrentUser endpoints not showing in documentation, may have been moved/removed?
-
- type RoleConnectionResource (httpClientFactory: IHttpClientFactory, token: string) =
+ type RoleConnectionResource (httpClientFactory, token) =
     interface IRoleConnectionResource with
-        member _.GetApplicationRoleConnectionMetadataRecords
-            applicationId =
-                Req.create
-                    HttpMethod.Get
-                    Constants.DISCORD_API_URL
-                    $"/applications/{applicationId}/role-connections/metadata"
-                |> Req.bot token
-                |> Req.send httpClientFactory
-                |> Res.json
+        member _.GetApplicationRoleConnectionMetadataRecords applicationId =
+            req {
+                get $"applications/{applicationId}/role-connections/metadata"
+                bot token
+            }
+            |> Http.send httpClientFactory
+            |> Task.mapT Http.toJson
 
-        member _.UpdateApplicationRoleConnectionMetadataRecords
-            applicationId ``type`` key name nameLocalizations description descriptionLocalizations =
-                Req.create
-                    HttpMethod.Put
-                    Constants.DISCORD_API_URL
-                    $"/applications/{applicationId}/role-connections/metadata"
-                |> Req.bot token
-                |> Req.json (
-                    Dto()
-                    |> Dto.property "type" ``type``
-                    |> Dto.property "key" key
-                    |> Dto.property "name" name
-                    |> Dto.propertyIf "name_localizations" nameLocalizations
-                    |> Dto.property "description" description
-                    |> Dto.propertyIf "description_localizations" descriptionLocalizations
-                    |> Dto.json
-                )
-                |> Req.send httpClientFactory
-                |> Res.json
-
-        member _.GetCurrentUserApplicationRoleConnection
-            applicationId token =
-                Req.create
-                    HttpMethod.Get
-                    Constants.DISCORD_API_URL
-                    $"/users/@me/applications/{applicationId}/role-connection"
-                |> Req.oauth token
-                |> Req.send httpClientFactory
-                |> Res.json
-
-        member _.UpdateCurrentUserApplicationRoleConnection
-            applicationId token platformName platformUsername ``type`` key name nameLocalizations description
-            descriptionLocalizations =
-                Req.create
-                    HttpMethod.Put
-                    Constants.DISCORD_API_URL
-                    $"/users/@me/applications/{applicationId}/role-connection"
-                |> Req.oauth token
-                |> Req.json (
-                    Dto()
-                    |> Dto.propertyIf "platform_name" platformName
-                    |> Dto.propertyIf "platform_username" platformUsername
-                    |> Dto.property
-                        "metadata"
-                        {
-                            Type = ``type``
-                            Key = key
-                            Name = name
-                            NameLocalizations = nameLocalizations
-                            Description = description
-                            DescriptionLocalizations = descriptionLocalizations
-                        }
-                    |> Dto.json
-                )
-                |> Req.send httpClientFactory
-                |> Res.json
+        member _.UpdateApplicationRoleConnectionMetadataRecords applicationId content =
+            req {
+                put $"applications/{applicationId}/role-connections/metadata"
+                bot token
+                payload content
+            }
+            |> Http.send httpClientFactory
+            |> Task.mapT Http.toJson
