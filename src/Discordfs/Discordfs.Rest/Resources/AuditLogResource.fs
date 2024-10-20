4d63b815425e6c9238f6a7d53fa785c4a8ec72b7
FinOCE <me@itsf.in>
2024-10-20 15:39:06 +1000

    Refactor audit log resource into module

--- src/Discordfs/Discordfs.Rest/Resources/AuditLogResource.fs
+++ src/Discordfs/Discordfs.Rest/Resources/AuditLogResource.fs
@@ -1,31 +1,41 @@
 ﻿namespace Discordfs.Rest.Resources
 
 open Discordfs.Rest.Common
+open Discordfs.Rest.Types
 open Discordfs.Types
-open System.Threading.Tasks
+open System.Net
+open System.Net.Http
 
-type IAuditLogResource =
-    // https://discord.com/developers/docs/resources/audit-log#get-guild-audit-log
-    abstract member GetGuildAuditLog:
-        guildId: string ->
-        userId: string option ->
-        actionType: AuditLogEventType option ->
-        before: string option ->
-        after: string option ->
-        limit: int option ->
-        Task<AuditLog>
+type GetGuildAuditLogResponse =
+    | Ok of AuditLog
+    | NotFound of ErrorResponse
+    | TooManyRequests of RateLimitResponse
+    | Other of HttpStatusCode
 
-type AuditLogResource (httpClientFactory, token) =
-    interface IAuditLogResource with
-        member _.GetGuildAuditLog guildId userId actionType before after limit =
+module AuditLog =
+    let getGuildAuditLog
+        (guildId: string)
+        (userId: string option)
+        (actionType: AuditLogEventType option)
+        (before: string option)
+        (after: string option)
+        (limit: int option)
+        botToken
+        (httpClient: HttpClient) =
             req {
-                get "guilds/{guildId}/audit-logs"
-                bot token
+                get $"guilds/{guildId}/audit-logs"
+                bot botToken
                 query "user_id" userId
                 query "action_type" (actionType >>. _.ToString())
                 query "before" before
                 query "after" after
                 query "limit" (limit >>. _.ToString())
             }
-            |> Http.send httpClientFactory
-            |> Task.mapT Http.toJson
+            |> httpClient.SendAsync
+            |> Task.mapT (fun res -> task {
+                match res.StatusCode with
+                | HttpStatusCode.OK -> return! Task.map GetGuildAuditLogResponse.Ok (Http.toJson res)
+                | HttpStatusCode.NotFound -> return! Task.map GetGuildAuditLogResponse.NotFound (Http.toJson res)
+                | HttpStatusCode.TooManyRequests -> return! Task.map GetGuildAuditLogResponse.TooManyRequests (Http.toJson res)
+                | status -> return GetGuildAuditLogResponse.Other status
+            })
