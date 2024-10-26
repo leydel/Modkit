f6960b286e7f96ccace83e86be3d09bdc6bdb50d
FinOCE <me@itsf.in>
2024-10-26 16:26:30 +1000

    Add support for webhook events

---
+++ src/Discordfs/Discordfs.Webhook/Payloads/WebhookEvent.fs
@@ -0,0 +1,36 @@
+﻿namespace Discordfs.Webhook.Payloads
+
+open Discordfs.Webhook.Types
+open System.Text.Json
+open System.Text.Json.Serialization
+
+[<JsonConverter(typeof<WebhookEventConverter>)>]
+type WebhookEvent =
+    | EntitlementCreate of WebhookEventPayload<EntitlementCreate>
+    | ApplicationAuthorized of WebhookEventPayload<ApplicationAuthorized>
+
+and WebhookEventConverter () =
+    inherit JsonConverter<WebhookEvent> ()
+
+    override __.Read (reader, typeToConvert, options) =
+        let success, document = JsonDocument.TryParseValue(&reader)
+        if not success then raise (JsonException())
+
+        let webhookType =
+            document.RootElement.GetProperty "data"
+            |> _.GetProperty("type")
+            |> _.GetString()
+            |> WebhookEventType.FromString
+
+        let json = document.RootElement.GetRawText()
+
+        match webhookType with
+        | Some ENTITLEMENT_CREATE -> EntitlementCreate <| Json.deserializeF json
+        | Some APPLICATION_AUTHORIZED -> ApplicationAuthorized <| Json.deserializeF json
+        | _ -> failwith "Unexpected WebhookEventType provided"
+                
+    override __.Write (writer, value, options) =
+        match value with
+        | EntitlementCreate e -> Json.serializeF e |> writer.WriteRawValue
+        | ApplicationAuthorized a -> Json.serializeF a |> writer.WriteRawValue
+        
---
+++ src/Discordfs/Discordfs.Webhook/Types/WebhookEventTypes.fs
@@ -0,0 +1,53 @@
+﻿namespace Discordfs.Webhook.Types
+
+open Discordfs.Types
+open System
+open System.Text.Json
+open System.Text.Json.Serialization
+
+// https://discord.com/developers/docs/events/webhook-events#webhook-types
+type WebhookType =
+    | PING = 0
+    | EVENT = 1
+
+// https://discord.com/developers/docs/events/webhook-events#event-types
+type WebhookEventType =
+    | APPLICATION_AUTHORIZED
+    | ENTITLEMENT_CREATE
+with
+    override this.ToString () =
+        match this with
+        | WebhookEventType.APPLICATION_AUTHORIZED -> "APPLICATION_AUTHORIZED"
+        | WebhookEventType.ENTITLEMENT_CREATE -> "ENTITLEMENT_CREATE"
+
+    static member FromString (str: string) =
+        match str with
+        | "APPLICATION_AUTHORIZED" -> Some WebhookEventType.APPLICATION_AUTHORIZED
+        | "ENTITLEMENT_CREATE" -> Some WebhookEventType.ENTITLEMENT_CREATE
+        | _ -> None
+
+// https://discord.com/developers/docs/events/webhook-events#event-body-object
+type WebhookEventBody<'a> = {
+    [<JsonPropertyName "type">] Type: WebhookEventType
+    [<JsonPropertyName "timestamp">] [<JsonConverter(typeof<Converters.UnixEpoch>)>] Timestamp: DateTime
+    [<JsonPropertyName "data">] Data: 'a
+}
+
+// https://discord.com/developers/docs/events/webhook-events#payload-structure
+type WebhookEventPayload<'a> = {
+    [<JsonPropertyName "version">] Version: int
+    [<JsonPropertyName "application_id">] ApplicationId: string
+    [<JsonPropertyName "type">] Type: WebhookType
+    [<JsonPropertyName "event">] Event: WebhookEventBody<'a>
+}
+
+// https://discord.com/developers/docs/events/webhook-events#application-authorized-application-authorized-structure
+type ApplicationAuthorized = {
+    [<JsonPropertyName "integration_type">] IntegrationType: ApplicationIntegrationType option
+    [<JsonPropertyName "user">] User: User
+    [<JsonPropertyName "scopes">] Scopes: OAuth2Scope list
+    [<JsonPropertyName "guild">] Guild: Guild option
+}
+
+// https://discord.com/developers/docs/events/webhook-events#entitlement-create-entitlement-create-structure
+type EntitlementCreate = Entitlement
