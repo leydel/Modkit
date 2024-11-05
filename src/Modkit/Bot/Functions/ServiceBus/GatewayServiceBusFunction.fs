42fddfba4455b8dde5596b4f56c2b1d74af382f8
FinOCE <me@itsf.in>
2024-11-05 12:42:44 +1000

    Create bot function app with service bus trigger logging messages

---
+++ src/Modkit/Bot/Functions/ServiceBus/GatewayServiceBusFunction.fs
@@ -0,0 +1,25 @@
+﻿namespace Modkit.Bot.Functions
+
+open Azure.Messaging.ServiceBus
+open Discordfs.Gateway.Payloads
+open Microsoft.Azure.Functions.Worker
+open Microsoft.Azure.WebJobs
+open Microsoft.Extensions.Logging
+open System.Text.Json
+
+type GatewayServiceBusFunction () =
+    [<Function(nameof GatewayServiceBusFunction)>]
+    member _.Run (
+        [<ServiceBusTrigger("%GatewayQueueName%", IsSessionsEnabled = true)>] message: ServiceBusReceivedMessage,
+        ctx: FunctionContext
+    ) = task {
+        let json = message.Body.ToString()
+        ctx.GetLogger().LogDebug json
+
+        match Json.deserializeF<GatewayReceiveEvent> json with
+        | GatewayReceiveEvent.MESSAGE_CREATE ({ Data = data }) ->
+            match data.Message.Content with
+            | Some content -> ctx.GetLogger().LogDebug $"{data.Message.Author.Username} said: {content}"
+            | None -> ctx.GetLogger().LogDebug "Message content intent is not enabled"
+        | _ -> ()
+    }
---
+++ src/Modkit/Bot/Program.fs
@@ -0,0 +1,32 @@
+﻿open Azure.Core.Serialization
+open Microsoft.Azure.Functions.Worker
+open Microsoft.Extensions.Configuration
+open Microsoft.Extensions.DependencyInjection
+open Microsoft.Extensions.Hosting
+open System.IO
+open System.Text.Json
+
+HostBuilder()
+    .ConfigureFunctionsWorkerDefaults(fun ctx builder ->
+        builder.Services.Configure(fun (workerOptions: WorkerOptions) ->
+            workerOptions.Serializer <- JsonObjectSerializer(Json.options)
+        ) |> ignore
+    )
+    .ConfigureAppConfiguration(fun builder ->
+        builder
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("local.settings.json", true)
+            .AddEnvironmentVariables()
+        |> ignore
+    )
+    .ConfigureLogging(fun logging -> ())
+    .ConfigureServices(fun ctx services -> 
+        services
+            .AddApplicationInsightsTelemetryWorkerService()
+            .ConfigureFunctionsApplicationInsights()
+        |> ignore
+    )
+    .Build()
+    .RunAsync()
+|> Async.AwaitTask
+|> Async.RunSynchronously
