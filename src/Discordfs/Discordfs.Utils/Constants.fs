53703bf4c369a5e68574291d6f93bcda2b509547
FinOCE <me@itsf.in>
2024-11-28 12:34:22 +1000

    Create utils project and implement snowflake and message format utility functions

---
+++ src/Discordfs/Discordfs.Utils/Constants.fs
@@ -0,0 +1,5 @@
+﻿[<AutoOpen>]
+module Discordfs.Utils.Constants
+
+[<Literal>]
+let DISCORD_EPOCH = 1420070400000L
---
+++ src/Discordfs/Discordfs.Utils/Snowflake.fs
@@ -0,0 +1,28 @@
+﻿module Discordfs.Utils.Snowflake
+
+open System
+
+/// Parse a snowflake to read its inner data.
+let tryParse (snowflake: string) =
+    match Int64.TryParse snowflake with
+    | false, _ -> None
+    | true, id ->
+        let increment = id &&& 0b00000000_00000000_00000000_00000000_00000000_00000000_00001111_11111111L
+        let workerId  = id &&& 0b00000000_00000000_00000000_00000000_00000000_00000001_11110000_00000000L
+        let processId = id &&& 0b00000000_00000000_00000000_00000000_00000000_00111110_00000000_00000000L
+        let timestamp = id &&& 0b11111111_11111111_11111111_11111111_11111111_11000000_00000000_00000000L
+
+        let dateTimestamp = DateTime.UnixEpoch.AddMilliseconds (float (DISCORD_EPOCH + timestamp))
+
+        Some {|
+            Increment = increment
+            WorkerId = workerId
+            ProcessId = processId
+            Timestamp = dateTimestamp
+        |}
+
+// Create a snowflake based on the given tiemstamp for use in pagination requests.
+let tryCreate (dateTimestamp: DateTime) =
+    match DateTimeOffset dateTimestamp |> _.ToUnixTimeMilliseconds() with
+    | unix when unix < DISCORD_EPOCH -> None
+    | unix -> Some ((unix - DISCORD_EPOCH) <<< 22)
