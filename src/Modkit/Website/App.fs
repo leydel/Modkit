c1c53147dd82a040ebb0a671b167d0d62aff4399
FinOCE <me@itsf.in>
2024-12-02 15:02:44 +1000

    Setup website project

---
+++ src/Modkit/Website/App.fs
@@ -0,0 +1,13 @@
+﻿module Modkit.Website.App
+
+open Browser.Dom
+open Feliz
+
+[<ReactComponent>]
+let App () = React.fragment [
+    Html.p "Hello world"
+]
+
+ReactDOM
+    .createRoot(document.getElementById "root")
+    .render(App())
