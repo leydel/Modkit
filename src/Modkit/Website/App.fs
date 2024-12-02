3ccd368081bf71c7bfbde793722484db2e6af133
Fin <me@itsf.in>
2024-12-02 15:16:06 +1000

    Deploy empty site to SWA (#53)
    * Setup website project
    
    * Add temporary workflow trigger for testing
    
    * Add cd into website project folder
    
    * Set vite root to parent folder
    
    * Set fable script paths to current folder
    
    * Remove hello world text and website push workflow trigger

---
+++ src/Modkit/Website/App.fs
@@ -0,0 +1,13 @@
+﻿module Modkit.Website.App
+
+open Browser.Dom
+open Feliz
+
+[<ReactComponent>]
+let App () = React.fragment [
+    Html.div []
+]
+
+ReactDOM
+    .createRoot(document.getElementById "root")
+    .render(App())
