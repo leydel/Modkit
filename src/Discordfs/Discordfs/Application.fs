4b02a05e8c4af20aaed9a805d8ce76a2f5593859
FinOCE <me@itsf.in>
2024-12-16 12:03:43 +1000

    Change activity instance into application module

--- src/Discordfs/Discordfs/Application.fs
+++ src/Discordfs/Discordfs/Application.fs
@@ -2,8 +2,8 @@
 
 open Discordfs.Rest
 
-module ActivityInstance =
-    let get applicationId instanceId client = task {
+module Application =
+    let getActivityInstance applicationId instanceId client = task {
         let! res = Rest.getApplicationActivityInstance applicationId instanceId client
         return DiscordResponse.toOption res
     }
